1d73defeb2701ea101be4ea1b99cc974fee3d03c
Nguyễn Khánh <khanh26122000@gmail.com>
2019-12-01 09:48:07 +0700

    create scroll up

--- site/layout.php
+++ site/layout.php
@@ -16,17 +16,17 @@
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
     <!-- MA5 Slider-->
-    <script src="<?=$URL_JS?>/jquery-ui.min.js"></script>
-    <link href="<?=$URL_CSS?>/ma5slider.min.css" rel="stylesheet" type="text/css">
-    <script src="<?=$URL_JS?>/ma5slider.min.js"></script>
+    <script src="<?= $URL_JS ?>/jquery-ui.min.js"></script>
+    <link href="<?= $URL_CSS ?>/ma5slider.min.css" rel="stylesheet" type="text/css">
+    <script src="<?= $URL_JS ?>/ma5slider.min.js"></script>
     <script>
-        $(window).on('load', function () {
+        $(window).on('load', function() {
             $('.ma5slider').ma5slider();
         });
     </script>
 
-    <link rel="stylesheet" href="<?=$URL_CSS?>/index.css">
-    <link rel="stylesheet" href="<?=$URL_CSS?>/viewProduct.css">
+    <link rel="stylesheet" href="<?= $URL_CSS ?>/index.css">
+    <link rel="stylesheet" href="<?= $URL_CSS ?>/viewProduct.css">
 </head>
 
 <body>
@@ -39,6 +39,38 @@
     <?php require $view_name; ?>
 
     <?php require 'components/footer.php' ?>
+
+
+    <div id="stop" class="scrollTop d-none d-md-block" style="position:fixed;bottom: 100px;right: 55px;">
+        <a href=""><i class="fa fa-angle-double-up"></i></a>
+    </div>
+
+    <script>
+        $(document).ready(function() {
+            var scrollTop = $(".scrollTop");
+
+            $(scrollTop).css("opacity", "0");
+
+
+            $(window).scroll(function() {
+                var topPos = $(this).scrollTop();
+
+                if (topPos > 100) {
+                    $(scrollTop).css("opacity", "1");
+
+                } else {
+                    $(scrollTop).css("opacity", "0");
+                    $(scrollTop).css("transition", "linear .3s");
+                }
+            });
+            $(scrollTop).click(function() {
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 800);
+                return false;
+            }); 
+        }); 
+    </script>
 
 </body>
 
